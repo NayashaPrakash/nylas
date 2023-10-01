1df3bfb8002a36d816da8b441537df2c70699f9b
NayashaPrakash <Nayashap01@gmail.com>
2023-10-02 00:06:34 +0530

    Update sentiment.mjs

--- sentiment.mjs
+++ sentiment.mjs
@@ -3,7 +3,7 @@ async function query(data) {
 	const response = await fetch(
 		"https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-sentiment-latest",
 		{
-			headers: { Authorization: "Bearer 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" },
+			headers: { Authorization: "Bearer {ACCESS_TOKEN}" },
 			method: "POST",
 			body: JSON.stringify(data),
 		}
